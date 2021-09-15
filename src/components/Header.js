ea963d46f6ef4a4ce88a903f4106cc8b0c05ba70
Burhan-Rashid <burhanuz30@gmail.com>
2021-09-15 23:11:00 +0530

    Added header with search,sort and filter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./Header.css"
+import { AiOutlineSearch } from "react-icons/ai"
 
 function Header() {
+    const [search, setSearch] = React.useState("");
+    const [sort, setSort] = React.useState("rating");
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="header">
+            <div className="search__div">
+                <AiOutlineSearch size={28} color="gray" />
+                <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            <div className="next__div">
+                <div className="sort">
+                    <h3>Sort By: </h3>
+                    <select value={sort}
+                        onChange={(e) => setSort(e.target.value)}>
+                        <option value="rating">Rating</option>
+                        <option value="pricelh">Price Low to High</option>
+                        <option value="pricehl">Price High to low</option>
+                        <option value="eta">ETA</option>
+                    </select>
+                </div>
+                <div >
+                    <button className="filter">Filter</button>
+                </div>
+            </div>
+
         </div>
     )
 }
